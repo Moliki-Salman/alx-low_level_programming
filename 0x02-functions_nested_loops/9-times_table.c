9e0b88c2a3bbb905541d73494f8302ce3d26465c
Moliki-Salman <salmanameenat@gmailcom>
2022-12-19 04:57:01 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - print the 9 times table
+ */
+void times_table(void)
+{
+	int num1, card, pro;
+
+	for (num = 0; num1 <= 9; num1++)
+	{
+		_putchar('0');
+
+		for (card = 1; card <= 9; card++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			pro = num1 * card;
+
+			if (pro <= 9)
+				_putchar(' ');
+			else
+				_putchar((pro / 10) + '0');
+			-putchar((pro % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
